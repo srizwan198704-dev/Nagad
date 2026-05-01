9d90324c166224449549c6ef923adcc153cbb932
SR Rizwan <srizwan198704@gmail.com>
2026-05-01 17:38:56 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
+        // আধুনিক অ্যান্ড্রয়েড বিল্ডের জন্য স্থিতিশীল ভার্সন
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
