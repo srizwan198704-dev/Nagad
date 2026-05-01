5206ad16fab596f64ccd2fd5d1199bda931bf980
SR Rizwan <srizwan198704@gmail.com>
2026-05-01 17:34:43 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,15 @@ buildscript {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
 }
 
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
