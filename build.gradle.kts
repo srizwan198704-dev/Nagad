22e863d11d3c714e51622fa26f7e5bcce829415b
SR Rizwan <srizwan198704@gmail.com>
2026-05-01 17:24:27 +0600

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
