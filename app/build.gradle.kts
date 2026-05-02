ee1a99f700c0fae6a27cc2a0aa925a762d7b7ee5
SR Rizwan <srizwan198704@gmail.com>
2026-05-03 00:53:26 +0600

    Update version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.konasl.nagad"
         minSdk = 24
         targetSdk = 35
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
